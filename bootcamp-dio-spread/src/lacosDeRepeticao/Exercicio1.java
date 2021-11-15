c3d431ee2c079ae4b166aefc9758c44239d39bfb
onofre <brunoonofre64@gmail.com>
2021-11-15 10:13:03 -0300

    feito o exercicio 1

---
+++ bootcamp-dio-spread/src/lacosDeRepeticao/Exercicio1.java
@@ -0,0 +1,19 @@
+package lacosDeRepeticao;
+
+import java.util.Iterator;
+
+public class Exercicio1 { 
+	
+	// Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+	
+	public static void main(String[] args) {
+		
+		int[] vet = { 1, 2, 3, 4, 5};
+		
+		for(int i = 0; i < vet.length; i++) {
+			System.out.println(vet[i]);
+		}
+		 
+	}
+
+}
