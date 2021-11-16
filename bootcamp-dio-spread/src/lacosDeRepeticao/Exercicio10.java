01be2502c76398566520d19dfca9e0766e7813f6
onofre <brunoonofre64@gmail.com>
2021-11-16 06:32:16 -0300

    feito exercicio10

--- bootcamp-dio-spread/src/lacosDeRepeticao/Exercicio10.java
+++ bootcamp-dio-spread/src/lacosDeRepeticao/Exercicio10.java
@@ -7,7 +7,10 @@ public class Exercicio10 {
 		int[] nums = new int[11];
 		
 		for (int i = 3; i < nums.length -1; i += 2) {
-			
+			if(i == 7) {
+				continue;
+			}
+			System.out.println(nums[i] = i);
 		 }
 
 
