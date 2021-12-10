54f16712cbcc080a0d2c6ff438564b0dd2f90f4c
onofre <brunoonofre64@gmail.com>
2021-12-10 07:11:03 -0300

    criados metodos size e isEmpty

---
+++ bootcamp-dio-spread/src/com/listaCircular/ListaCircular.java
@@ -0,0 +1,22 @@
+package com.listaCircular;
+
+public class ListaCircular<T> {
+	
+	private No<T> cabeca;
+	private No<T> cauda;
+	private int tamanhoLista;
+	
+	
+	public int size() {
+		return this.tamanhoLista;
+	}
+	
+	
+	public boolean isEmpty() {
+		return this.tamanhoLista == 0 ? true : false;
+	}
+	
+	
+	
+
+}
